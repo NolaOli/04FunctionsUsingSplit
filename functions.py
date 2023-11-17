f27c58bf734ba04913690e1510ed99bef18c1e38
Nola Olien <25nolaolien@usd266.com>
2023-11-17 10:12:59 -0600

    strings changed to integers, added split

---
+++ functions.py
@@ -0,0 +1,52 @@
+def meal_time(timestring):
+  hours, minutes = timestring.split(":")
+
+  hours = int(hours) 
+  minutes = int(minutes)
+
+  if hours >= 7 and hours <= 8:
+    return "Breakfast time"
+  elif hours >= 12 and hours <= 13:
+    return "Lunch time"
+  elif hours >= 18 and hours <= 19:
+    return "Dinner time"
+  else:
+    return "No meal time currently."
+
+print("07:00 =>", meal_time("07:30"))
+print("18:00 =>", meal_time("18:30"))
+print("08:00 =>", meal_time("08:30"))
+print("13:00 =>", meal_time("13:15"))
+print("19:00 =>", meal_time("19:25"))
+print("14:00 =>", meal_time("14:35"))
+
+
+def get_filename_extension(filename):
+  file, extension = filename.split(".")
+
+  file = file
+  extension = extension
+  
+  return extension
+
+print("data.csv =>", get_filename_extension("data.csv"))
+print("woop.pop =>", get_filename_extension("woop.pop"))
+print("wopy.kip =>", get_filename_extension("wopy.kip"))
+
+
+def is_image_file(filename):
+  file, extension = filename.split(".")
+
+  file = file
+  extension = extension
+
+  if extension == "jpg" and extension == "jpeg" and extension == "png" and extension == "gif":
+    return True
+  elif extension == "tiff":
+    return True
+  else:
+    return "Not an image file."
+
+print("skippy.jpg =>", is_image_file("skippy.jpg"))
+print("skop.csv =>", is_image_file("skop.csv"))
+print("tiffiany.tiff =>", is_image_file("tiffiany.tiff"))
